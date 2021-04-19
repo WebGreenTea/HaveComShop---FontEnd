df722f0a721bd1ac10bf64ec563981869fbe0f98
WebGreenTea <sahawut092013@gmail.com>
2021-04-19 16:50:55 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   >
     <div class="container">
       <router-link to="/" class="navbar-brand float-left"
-        >HaveCOM SHOP TEST</router-link
+        >HaveCOM SHOP</router-link
       >
       <div class="collapse navbar-collapse topnav-right" id="navbarDropdown">
         <ul class="nav navbar-nav ml-auto" v-if="Inlogin">
